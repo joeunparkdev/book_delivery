6c166480f5e5cfbb937e7d5ca8c20242b4401d0b
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-11-15 00:45:25 +0900

    테스트 파일 작성-회원가입 추가해야함

--- tests/api/test.js
+++ tests/api/test.js
@@ -1,75 +1,222 @@
-
 async function getAccessToken() {
-    try {
-        let loginHeaders = {
-            "Accept": "*/*",
-            "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-            "Content-Type": "application/json"
-        };
+  try {
+    let loginHeaders = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      "Content-Type": "application/json",
+    };
 
-        let loginBody = JSON.stringify({
-            "username": "ssssd@dsgs.com",
-            "password": "000000"
-        });
+    let loginBody = JSON.stringify({
+      username: "ssssd@dsgs.com",
+      password: "000000",
+    });
 
-        let loginResponse = await fetch("http://localhost:3000/api/auth/login", {
-            method: "POST",
-            body: loginBody,
-            headers: loginHeaders
-        });
+    let loginResponse = await fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      body: loginBody,
+      headers: loginHeaders,
+    });
 
-        if (!loginResponse.ok) {
-            throw new Error("Failed to log in. Status: " + loginResponse.status);
-        }
-
-        let accessTokenData = await loginResponse.json();
-        if (!accessTokenData || !accessTokenData.accessToken) {
-            throw new Error("No valid access token available. Token data: " + JSON.stringify(accessTokenData));
-        }
-
-        return accessTokenData.accessToken;
-    } catch (error) {
-        console.error("Error in getAccessToken:", error);
-        throw error;
+    if (!loginResponse.ok) {
+      throw new Error("Failed to log in. Status: " + loginResponse.status);
     }
+
+    let accessTokenData = await loginResponse.json();
+    if (!accessTokenData || !accessTokenData.accessToken) {
+      throw new Error(
+        "No valid access token available. Token data: " +
+          JSON.stringify(accessTokenData),
+      );
+    }
+
+    return accessTokenData.accessToken;
+  } catch (error) {
+    console.error("Error in getAccessToken:", error);
+    throw error;
+  }
 }
 
 async function getProductList(accessToken) {
-       console.log(accessToken);
-    try {
-        let productListHeaders = {
-            "Accept": "*/*",
-            "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-            "Authorization": "Bearer " + accessToken
-        };
+  try {
+    let productListHeaders = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      Authorization: "Bearer " + accessToken,
+    };
 
-        let productListResponse = await fetch("http://localhost:3000/api/products/list", {
-            method: "GET",
-            headers: productListHeaders
-        });
+    let productListResponse = await fetch(
+      "http://localhost:3000/api/products/list",
+      {
+        method: "GET",
+        headers: productListHeaders,
+      },
+    );
 
-        if (!productListResponse.ok) {
-            throw new Error("Failed to fetch the product list. Status: " + productListResponse.status);
-        }
-
-        return productListResponse.json();
-    } catch (error) {
-        console.error("Error in getProductList:", error);
-        throw error;
+    if (!productListResponse.ok) {
+      throw new Error(
+        "Failed to fetch the product list. Status: " +
+          productListResponse.status,
+      );
     }
+
+    return productListResponse.json();
+  } catch (error) {
+    console.error("Error in getProductList:", error);
+    throw error;
+  }
+}
+
+async function getProduct(accessToken, productId) {
+  try {
+    let headersList = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      Authorization: "Bearer " + accessToken,
+    };
+
+    let response = await fetch(
+      "http://localhost:3000/api/products/list/" + productId,
+      {
+        method: "GET",
+        headers: headersList,
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        "Failed to fetch the product. Status: " + response.status,
+      );
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error in getProduct:", error);
+    throw error;
+  }
+}
+
+async function createProduct(accessToken) {
+    try {
+      let headersList = {
+        Accept: "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        Authorization: "Bearer " + accessToken,
+        "Content-Type": "application/json",
+      };
+  
+      let bodyContent = JSON.stringify({
+        title: "아이폰15 MAX",
+        content: "얼마 사용하지 않은 제품 팝니다.",
+      });
+  
+      let response = await fetch("http://localhost:3000/api/products/create", {
+        method: "POST",
+        body: bodyContent,
+        headers: headersList,
+      });
+  
+      if (!response.ok) {
+        throw new Error(
+          "Failed to create the product. Status: " + response.status,
+        );
+      }
+      return response.json();
+
+    } catch (error) {
+      console.error("Error in creating the product:", error);
+      throw error;
+    }
+  }
+  
+async function modifyProduct(accessToken, productId) {
+  try {
+    let headersList = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      Authorization: "Bearer " + accessToken,
+      "Content-Type": "application/json",
+    };
+
+    let bodyContent = JSON.stringify({
+      title: "아이폰14 MAX",
+      content: "얼마 사용하지 않은 제품 팝니다.",
+    });
+
+    let response = await fetch(
+      "http://localhost:3000/api/products/modify/" + productId,
+      {
+        method: "PUT",
+        body: bodyContent,
+        headers: headersList,
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        "Failed to fetch the product. Status: " + response.status,
+      );
+    }
+
+    return await response.json();
+
+  } catch (error) {
+    console.error("Error in modifying the product:", error);
+    throw error;
+  }
+}
+async function deleteProduct(accessToken, productId) {
+  try {
+    let headersList = {
+      Accept: "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      Authorization: "Bearer " + accessToken,
+    };
+
+    let response = await fetch(
+      "http://localhost:3000/api/products/delete/" + productId,
+      {
+        method: "DELETE",
+        headers: headersList,
+      },
+    );
+    if (!response.ok) {
+      throw new Error(
+        "Failed to fetch the product. Status: " + response.status,
+      );
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error in deleting the product:", error);
+    throw error;
+  }
 }
 
 // Main function
 async function main() {
-    try {
-        const accessToken = await getAccessToken();
-        console.log("Access Token:", accessToken);
+  try {
+    const accessToken = await getAccessToken();
+    console.log("Access Token:", accessToken);
 
-        const productList = await getProductList(accessToken);
-        console.log("Product List:", productList);
-    } catch (error) {
-        console.error("Error:", error);
-    }
+    const productList = await getProductList(accessToken);
+    console.log("Product List:", productList);
+
+    const createdProduct = await createProduct(accessToken);
+    console.log("Created Product:", createdProduct);
+    const productId = createdProduct.productId;
+
+    const product = await getProduct(accessToken,  productId);
+    console.log("Retreived Product:", product);
+
+    const modifiedProduct = await modifyProduct(accessToken,  productId);
+    console.log("Modified Product:", modifiedProduct );
+
+    const deletedProduct = await deleteProduct(accessToken,  productId);
+    console.log("Deleted Product:", deletedProduct);
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 // Call the main function
