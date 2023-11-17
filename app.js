78dc4ee2e463dca576b26d84429aadc9586e438c
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-11-17 20:06:29 +0900

    refacotring

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const authMiddleware = require("./middlewares/auth-middleware.js");
 const productsRouter = require("./routers/products.router.js");
 const usersRouter = require("./routers/users.router.js");
 const authRouter = require("./routers/auth.router.js");
 const app = express();
 
-app.use(express.static("assets"));
-app.use(express.json()); //바디에 가진 데이터 사용
+app.use(express.json()); //body parser
 app.use(express.urlencoded({ extended: false })); //FORM 처리
 
 app.use("/api/auth", authRouter);
-app.use("/api/users", authMiddleware, usersRouter);
-app.use("/api/products", authMiddleware, productsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/products", productsRouter);
 
 const PORT = process.env.PORT || 3000;
 
