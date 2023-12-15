5bd0a1c473cb866aaf9fc2dc5938587e32fd78c6
mokiebetts <145108127+mokiebetts@users.noreply.github.com>
2023-12-15 12:12:32 +0900

    Revert "Feature/takeorder prisma order 수정 bookstore api수정 "

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,29 +1,29 @@
-import express from 'express'
-import passport from 'passport'
-import { AuthController } from '../controllers/auth.controller.js'
-import { authMiddleware } from '../middlewares/auth-middleware.js'
+import express from "express";
+import passport from "passport";
+import { AuthController } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth-middleware.js";
 
-const router = express.Router()
-const authController = new AuthController()
+const router = express.Router();
+const authController = new AuthController();
 
-router.post('/signup/:userType', authController.signUp)
-router.post('/verify', authController.verify)
-router.post('/sendCode', authController.sendCode)
-router.get('/check-email', authController.checkEmailExists)
-router.post('/signin', authController.signIn)
-router.post('/signout', authMiddleware, authController.signOut)
+router.post("/signup/:userType", authController.signUp);
+router.post("/verify", authController.verify);
+router.post("/sendCode", authController.sendCode);
+router.get("/check-email", authController.checkEmailExists);
+router.post("/signin", authController.signIn);
+router.post("/signout", authMiddleware, authController.signOut);
 
-router.get('/kakao', passport.authenticate('kakao', { session: false }))
+router.get("/kakao", passport.authenticate("kakao", { session: false }));
 try {
   router.get(
-    '/kakao/callback',
-    passport.authenticate('kakao', {
+    "/kakao/callback",
+    passport.authenticate("kakao", {
       session: false,
     }),
     authController.kakaoLogin,
-  )
+  );
 } catch (error) {
-  console.log(error)
+  console.log(error);
 }
 
-export default router
+export default router;
