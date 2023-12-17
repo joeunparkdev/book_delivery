d3faf3aabf54379d420736287b9e0ca1b76ff82f
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-12-17 10:01:14 +0900

    review debug

--- src/assets/js/book.detail.js
+++ src/assets/js/book.detail.js
@@ -1,3 +1,87 @@
+function addReview() {
+  const productIdInput = document.getElementById("productIdInput");
+  const ratingSelect = document.getElementById("starRating");
+  const reviewTextInput = document.querySelector(".review-wr textarea");
+
+  if (productIdInput && ratingSelect && reviewTextInput) {
+    const productId = productIdInput.value;
+    const rating = parseInt(ratingSelect.value);
+    const reviewText = reviewTextInput.value;
+
+    submitReview(productId, rating, reviewText);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const starRatingSelect = document.getElementById("starRating");
+
+  function updateStarRating() {
+    const selectedRating = parseInt(starRatingSelect.value, 10);
+    const starRatingContainer = document.getElementById("starRatingContainer");
+    if (starRatingContainer) {
+      starRatingContainer.innerHTML = "★".repeat(selectedRating);
+    }
+  }
+
+  updateStarRating();
+
+  if (starRatingSelect) {
+    starRatingSelect.addEventListener("input", updateStarRating);
+  }
+
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get("id");
+    const productDetails = await fetchProductDetails(productId);
+    const reviews = await displayReviews(productId);
+
+    const productDetailElement = document.getElementById("productDetail");
+
+    if (!productDetailElement.innerHTML.trim()) {
+      displayProductDetails(productDetails);
+    }
+    console.log(productId);
+    displayReviews(productId);
+    updateStarRating();
+  } catch (error) {
+    console.error("에러 ---", error);
+  }
+
+  // Add event listener to the addReviewBtn
+  const addReviewBtn = document.getElementById("addReviewBtn");
+  if (addReviewBtn) {
+    addReviewBtn.addEventListener("click", addReview);
+  }
+});
+
+async function submitReview(productId, rating, reviewText) {
+  try {
+    const url = `/api/review/${productId}`;
+    const requestBody = {
+      rating: rating,
+      reviewText: reviewText,
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`리뷰 제출 실패. HTTP 오류! 상태: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("리뷰가 성공적으로 제출되었습니다:", data);
+  } catch (error) {
+    console.error("리뷰 제출 중 에러 발생:", error.message);
+  }
+}
+
 function displayProductDetails(product) {
   const productDetailElement = document.getElementById("productDetail");
   displayReviews(product.reviews);
@@ -20,50 +104,57 @@ function displayProductDetails(product) {
   productDetailElement.appendChild(descriptionElement);
 }
 
-function displayReviews(reviews) {
-  const reviewListElement = document.getElementById("reviewList");
-  reviewListElement.innerHTML = "";
-
-  if (reviews && reviews.length > 0) {
-    for (const review of reviews) {
-      const reviewElement = document.createElement("div");
-      reviewElement.className = "review";
-
-      const reviewerElement = document.createElement("div");
-      reviewerElement.textContent = `리뷰어: ${review.reviewer}`;
-      reviewElement.appendChild(reviewerElement);
-
-      const contentElement = document.createElement("div");
-      contentElement.textContent = `리뷰 내용: ${review.content}`;
-      reviewElement.appendChild(contentElement);
-
-      const ratingElement = document.createElement("div");
-      ratingElement.textContent = "평점: ";
-      for (let i = 0; i < review.rating; i++) {
-        const starElement = document.createElement("span");
-        starElement.textContent = "★";
-        ratingElement.appendChild(starElement);
-      }
-      reviewElement.appendChild(ratingElement);
-
-      reviewListElement.appendChild(reviewElement);
+async function displayReviews(productId) {
+  try {
+    const response = await fetch(`/api/review/${productId}`);
+    if (!response.ok) {
+      throw new Error(`HTTP 오류! 상태: ${response.status}`);
     }
-  } else {
-    const noReviewsElement = document.createElement("p");
-    noReviewsElement.textContent = "리뷰가 없습니다.";
-    reviewListElement.appendChild(noReviewsElement);
+    const reviews = await response.json();
+
+    const reviewListElement = document.getElementById("reviewList");
+    reviewListElement.innerHTML = "";
+
+    if (reviews && reviews.length > 0) {
+      for (const review of reviews) {
+        const reviewElement = document.createElement("div");
+        reviewElement.className = "review";
+
+        const reviewerElement = document.createElement("div");
+        reviewerElement.textContent = `리뷰어: ${review.reviewer}`;
+        reviewElement.appendChild(reviewerElement);
+
+        const contentElement = document.createElement("div");
+        contentElement.textContent = `리뷰 내용: ${review.content}`;
+        reviewElement.appendChild(contentElement);
+
+        const ratingElement = document.createElement("div");
+        ratingElement.textContent = "평점: ";
+        for (let i = 0; i < review.rating; i++) {
+          const starElement = document.createElement("span");
+          starElement.textContent = "★";
+          ratingElement.appendChild(starElement);
+        }
+        reviewElement.appendChild(ratingElement);
+
+        reviewListElement.appendChild(reviewElement);
+      }
+    } else {
+      const noReviewsElement = document.createElement("p");
+      noReviewsElement.textContent = "리뷰가 없습니다.";
+      reviewListElement.appendChild(noReviewsElement);
+    }
+  } catch (error) {
+    console.error("리뷰 가져오기 중 에러 발생:", error.message);
   }
 }
 
-async function getUserId() {
-  // 사용자 ID를 가져오는 비동기 함수 구현
-}
-
 async function fetchProductDetails(productId) {
+  console.log(productId);
   try {
     const response = await fetch(`/api/products/${productId}`);
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`HTTP 오류! 상태: ${response.status}`);
     }
     const data = await response.json();
     return data.data;
--- src/assets/js/myPage.js
+++ src/assets/js/myPage.js
@@ -1,58 +1,70 @@
-// 프로필 삭제: TODO
 async function deleteProfile() {
+  console.log("Before confirm");
+  const confirmed = confirm("정말로 이 서점을 삭제하시겠습니까?");
+  console.log("After confirm");
+  if (confirmed) {
     try {
-        const response = await fetch(`/api/users/me`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: "include",
-        });
+      const response = await fetch(`/api/users/me`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
-        if (response.ok) {
-            const result = await response.json();
-            // 삭제 후 페이지 갱신
-            location.reload();
-        } else if (response.status === 403) {
-            // 권한이 없는 경우 알림 표시
-            alert('삭제할 권한이 없습니다.');
-        } else {
-            console.error('Error deleting profile:', response.status, response.statusText);
-            const errorData = await response.json(); // Log additional error details
-            console.error('Additional error details:', errorData);
-        }
+      if (response.ok) {
+        const result = await response.json();
+        alert("서점이 성공적으로 삭제되었습니다.");
+      } else if (response.status === 403) {
+        // 권한이 없는 경우 알림 표시
+        alert("삭제할 권한이 없습니다.");
+      } else {
+        console.error(
+          "Error deleting profile:",
+          response.status,
+          response.statusText,
+        );
+        const errorData = await response.json();
+        console.error("Additional error details:", errorData);
+      }
     } catch (error) {
-        console.error('Error deleting profile:', error.message);
+      console.error("Error deleting profile:", error.message);
     }
+  }
 }
 
 // 프로필 가져오기
 async function getUserDetails() {
-    try {
-            const response = await fetch(`/api/users/me`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: "include",
-            });
+  try {
+    const response = await fetch(`/api/users/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-            if (response.ok) {
-                const result = await response.json();
-                const userData = result.data; 
+    if (response.ok) {
+      const result = await response.json();
+      const userData = result.data;
 
-                // Update HTML with user details
-                document.getElementById('userName').innerText = `이름: ${userData.username}`;
-                document.getElementById('userEmail').innerText = `Email: ${userData.email}`;
-            } else if (response.status === 403) {
-                alert('조회할 권한이 없습니다.');
-            } else {
-                console.error('Error fetching user profile:', response.status, response.statusText);
-            }
-        
-    } catch (error) {
-        console.error('Error fetching user profile:', error.message);
+      // Update HTML with user details
+      document.getElementById("userName").innerText =
+        `이름: ${userData.username}`;
+      document.getElementById("userEmail").innerText =
+        `Email: ${userData.email}`;
+    } else if (response.status === 403) {
+      alert("조회할 권한이 없습니다.");
+    } else {
+      console.error(
+        "Error fetching user profile:",
+        response.status,
+        response.statusText,
+      );
     }
+  } catch (error) {
+    console.error("Error fetching user profile:", error.message);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', getUserDetails);
+document.addEventListener("DOMContentLoaded", getUserDetails);
